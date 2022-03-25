7936ddf87e3ce61718aae2fd96251a2225d1510a
SanJaYrOG <sanjay.soni@theporter.in>
2022-03-25 14:52:59 +0530

    cicd terraform

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region     = "ap-southeast-1"
+}
+
+resource "aws_instance" "my-first-server" {
+  ami = "ami-055d15d9cfddf7bd3"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Ubuntu"
+  }
+}
