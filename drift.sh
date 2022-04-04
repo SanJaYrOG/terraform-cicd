16ab73b6bb5d1816ccc98f236f5647fc603e17f4
SanJaYrOG <98605487+SanJaYrOG@users.noreply.github.com>
2022-04-04 18:20:37 +0530

    Create drift.sh

---
+++ drift.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+terraform plan --detailed-exitcode -out=tf.plan 2> /dev/null || ec=$?
+case $ec in 
+0) echo "No Changes Found": exit 0;;
+1) printf '%s/n' "Command exited with non-zero";exit 1;;
+2) echo "Changes found";
+   MESSAGE=$(terraform show -no-color tf.plan| awk '/#/,EOF { print $0 }');
+   curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" <https://hooks.slack.com/services/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>
+esac
