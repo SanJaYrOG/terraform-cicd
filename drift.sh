4e5d4d16c879ccc632a6c9a14ed1cfc2b992bc94
SanJaYrOG <98605487+SanJaYrOG@users.noreply.github.com>
2022-04-05 12:41:41 +0530

    Update drift.sh

--- drift.sh
+++ drift.sh
@@ -5,5 +5,5 @@ case $ec in
 1) printf '%s/n' "Command exited with non-zero";exit 1;;
 2) echo "Changes found";
    MESSAGE=$(terraform show -no-color tf.plan| awk '/#/,EOF { print $0 }');
-   curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" <xoxb-3439815405-3345921521140-8fklfYtDnxn9L8aTpFFwlx1s>
+   curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" <https://hooks.slack.com/services/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>
 esac
