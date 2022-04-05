280efd1d4973295f1d30df602a2db75ad68386da
SanJaYrOG <98605487+SanJaYrOG@users.noreply.github.com>
2022-04-05 15:45:33 +0530

    Update drift.sh

--- drift.sh
+++ drift.sh
@@ -3,5 +3,5 @@ SLACK_WEBHOOK_URL=$1
 terraform plan -out=plan_out
 MESSAGE="$(terraform show plan_out)"
 echo $MESSAGE
-curl -X POST "${SLACK_WEBHOOK_URL}" -H 'Content-type: application/json' --data "{'text': ${MESSAGE}}"
+curl -X POST -H 'Content-type: application/json' --data "{'text': "$MESSAGE"}" "${SLACK_WEBHOOK_URL}" 
                               
