cbdd34fd859f45c362429c11671ed73bea1461c4
SanJaYrOG <98605487+SanJaYrOG@users.noreply.github.com>
2022-04-05 13:07:44 +0530

    Update drift.sh

--- drift.sh
+++ drift.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+SLACK_WEBHOOK_URL="$1"
 terraform plan --detailed-exitcode -out=tf.plan 2> /dev/null || ec=$?
 case $ec in 
 0) echo "No Changes Found": exit 0;;
 1) printf '%s/n' "Command exited with non-zero";exit 1;;
 2) echo "Changes found";
    MESSAGE=$(terraform show -no-color tf.plan| awk '/#/,EOF { print $0 }');
-   curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" <https://hooks.slack.com/services/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>
+   curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" ${SLACK_WEBHOOK_URL}
 esac
